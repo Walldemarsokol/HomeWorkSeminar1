2cb95e8721a56b294c13bf1b520426e2bfa0f6f9
VladimirSokol <walldemarsokol@yandex.ru>
2022-08-20 23:23:22 +0300

    add Test2 to folder

---
+++ Test2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.Write("Введите первое число ");
+int number1 = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число ");
+int number2 = int.Parse(Console.ReadLine());
+
+if(number1 > number2)
+{
+    Console.WriteLine($"Наибольшее число это {number1}");
+}
+else
+{
+    Console.WriteLine($"Наибольшее число это {number2}");
+}
